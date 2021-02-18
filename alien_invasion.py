eb7df4ce71e77fc614fc5b09bc220aca6079525e
Joker6934 <3043565604@qq.com>
2021-02-18 22:26:33 +0800

    third commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -12,18 +14,20 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()  # 这个不用是因为我在settings里设置了screen，就是原始的屏幕
         self.screen = self.settings.screen  # 这边必须要先定义screen，不然ship的__init__用不了
-        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # 这是可以全局游戏的设置，但是我的背景图片不能
-        # 全覆盖，而且出现了BUG，不能用
-        # self.settings.screen_width = self.screen.get_rect().width
-        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -44,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_key_up_event(self, event):
         """响应松开按键"""
@@ -52,12 +58,74 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组"""
+        if len(self.bullets) < self.settings.bullets_allowed:  # 屏幕上的子弹少于限额才能发射子弹
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """更新屏幕上的图集，并切换到新的屏幕"""
         self.settings.build_screen()  # 每次循环都重绘屏幕
         self.ship.blit_me()  # 放置飞船
+        for bullet in self.bullets.sprites():  # 绘制子弹
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)  # 放置外星人
         # 让绘制的屏幕可见
         pygame.display.flip()
+
+    def _update_bullets(self):
+        """更新子弹位置并删除消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # 检测是否有子弹击中了外星人，是，则删除子弹和外星人
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    def _update_aliens(self):
+        """更新外星人的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _create_fleet(self):
+        """创建外星人群"""
+        # 创建一个外星人并计算一行可以容纳多少个外星人
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen.get_rect().width - 2 * alien_width
+        number_aliens_x = available_space_x // (2 * alien_width)  # 间距为外星人的宽度
+        # 计算屏幕可以容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen.get_rect().height - 3 * alien_height - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+        # 创建一群外星人
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """创建一个外星人，并将其放在当前行"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """有外星人到达边缘时，采取措施"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -6,12 +6,24 @@ class Settings:
 
     def __init__(self):
         """初始化游戏设置"""
-        # 屏幕设置--背景长，宽和背景
-        self.size = (1200, 700)  # 设置屏幕大小
-        self.screen = pygame.display.set_mode(self.size)
+        # 屏幕设置--背景长，宽和背景图片
+        # self.size = (1200, 700)  # 设置屏幕大小,这个是小窗口
+        # self.screen = pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.background = pygame.image.load('images/background.jpg')
-        # 飞船设置
-        self.ship_speed = 2
+        # 飞船速度设置
+        self.ship_speed = 1.5
+        # 子弹速度设置
+        self.bullet_speed = 1.0
+        # 子弹上限设置，屏幕上最多有10颗子弹
+        self.bullets_allowed = 10
+        # 外星人速度
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction 为1表示向右移动，-1表示向左移动
+        self.fleet_direction = 1
 
     def build_screen(self):
-        self.screen.blit(pygame.transform.scale(self.background, self.size), (0, 0))  # 伸缩背景
+        self.screen.blit(pygame.transform.scale(self.background,
+                                                (self.screen.get_rect().width, self.screen.get_rect().height)),
+                         (0, 0))  # 伸缩背景
